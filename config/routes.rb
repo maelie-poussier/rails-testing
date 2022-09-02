0453e7f8fdb9318969eff0b9e6ce9070e5848f76
maelie poussier <maelie.poussier@icloud.com>
2022-09-02 10:02:23 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :products, only: :index
   # Defines the root path route ("/")
+  resources :products, only: [:new, :create]
   # root "articles#index"
 end
